2ce7cdda189f6cf65281ef715124cb89df3c19d0
zqq90 <zqq_90@163.com>
2013-08-21 11:17:44 +0800

    Loaders use engine.encoding as default

--- webit-script/src/main/java/webit/script/loaders/AbstractLoader.java
+++ webit-script/src/main/java/webit/script/loaders/AbstractLoader.java
@@ -4,12 +4,14 @@ package webit.script.loaders;
 
 import jodd.io.FileNameUtil;
 import jodd.util.StringUtil;
+import webit.script.Configurable;
+import webit.script.Engine;
 
 /**
  *
  * @author Zqq
  */
-public abstract class AbstractLoader implements Loader {
+public abstract class AbstractLoader implements Loader, Configurable {
 
     protected String encoding;
     protected String root = null;
@@ -54,5 +56,11 @@ public abstract class AbstractLoader implements Loader {
 
     public void setEncoding(String encoding) {
         this.encoding = encoding;
+    }
+
+    public void init(Engine engine) {
+        if (encoding == null) {
+            encoding = engine.getEncoding();
+        }
     }
 }
