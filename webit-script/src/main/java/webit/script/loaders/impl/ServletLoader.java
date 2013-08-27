5a5c096d8610b3693cc6674f7a3875b7e5c46eca
zqq90 <zqq_90@163.com>
2013-08-27 21:08:01 +0800

    ServletLoader

--- webit-script/src/main/java/webit/script/loaders/impl/ServletLoader.java
+++ webit-script/src/main/java/webit/script/loaders/impl/ServletLoader.java
@@ -1,8 +1,9 @@
 // Copyright (c) 2013, Webit Team. All Rights Reserved.
-
 package webit.script.loaders.impl;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import webit.script.exceptions.ResourceNotFoundException;
 import webit.script.loaders.AbstractLoader;
 import webit.script.loaders.Resource;
@@ -12,12 +13,23 @@ import webit.script.loaders.impl.resources.FileResource;
  *
  * @author Zqq
  */
-public class ServletLoader extends AbstractLoader {
+public class ServletLoader extends AbstractLoader implements ServletContextListener {
 
     private ServletContext servletContext;
 
     public Resource get(String name) throws ResourceNotFoundException {
         String realpath = servletContext.getRealPath(getRealPath(name));
+        if (realpath == null) {
+            throw new ResourceNotFoundException("Not found: " + name);
+        }
         return new FileResource(realpath, encoding);
+    }
+
+    public void contextInitialized(ServletContextEvent sce) {
+        servletContext = sce.getServletContext();
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+        servletContext = null;
     }
 }
