31354f38bab399585f03b5a1992c3e54a970501b
zqq90 <zqq_90@163.com>
2013-08-27 21:11:18 +0800

    ClasspathResource Exception improved

--- webit-script/src/main/java/webit/script/loaders/impl/resources/ClasspathResource.java
+++ webit-script/src/main/java/webit/script/loaders/impl/resources/ClasspathResource.java
@@ -1,11 +1,11 @@
 // Copyright (c) 2013, Webit Team. All Rights Reserved.
-
 package webit.script.loaders.impl.resources;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import webit.script.exceptions.ResourceNotFoundException;
 import webit.script.loaders.Resource;
 
 /**
@@ -30,7 +30,9 @@ public class ClasspathResource implements Resource {
     public Reader openReader() throws IOException {
         //lastLoadTime = System.currentTimeMillis();
         InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-
+        if (inputStream == null) {
+            throw new ResourceNotFoundException("Not found: " + path);
+        }
         return encoding == null
                 ? new InputStreamReader(inputStream)
                 : new InputStreamReader(inputStream, encoding);
