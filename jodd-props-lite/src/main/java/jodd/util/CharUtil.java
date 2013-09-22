cb26932f3acd093529380e80567b24144d6da994
Zqq <zqq_90@163.com>
2013-09-22 15:46:52 +0800

    Added Jodd-props lite

---
+++ jodd-props-lite/src/main/java/jodd/util/CharUtil.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2013, Webit Team. All Rights Reserved.
+package jodd.util;
+
+/**
+ *
+ * @author Zqq
+ */
+public class CharUtil {
+    
+    public static boolean isWhitespace(char c) {
+        return c <= ' ';
+    }
+}
