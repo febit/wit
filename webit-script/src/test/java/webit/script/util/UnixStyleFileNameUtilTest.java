2a62f1ca85585b95c30cd84212de01a09e1c4438
Zqq <zqq_90@163.com>
2013-09-23 15:13:45 +0800

    Fixed UnixStyleFileNameUtil.getPath(String) bug

---
+++ webit-script/src/test/java/webit/script/util/UnixStyleFileNameUtilTest.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2013, Webit Team. All Rights Reserved.
+package webit.script.util;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Zqq
+ */
+public class UnixStyleFileNameUtilTest {
+
+    @Test
+    public void test() {
+        assertEquals("/parent/tmpl.wtl", UnixStyleFileNameUtil.concat("/parent/", "tmpl.wtl"));
+        assertEquals("/parent/tmpl.wtl", UnixStyleFileNameUtil.concat("/parent/", "./tmpl.wtl"));
+        assertEquals("/tmpl.wtl", UnixStyleFileNameUtil.concat("/parent/", "../tmpl.wtl"));
+        
+        
+        assertEquals("/tmpl.wtl", UnixStyleFileNameUtil.normalize("/parent/../tmpl.wtl"));
+        
+        assertNull(UnixStyleFileNameUtil.normalize("/../tmpl.wtl"));
+        assertNull(UnixStyleFileNameUtil.normalize("../tmpl.wtl"));
+        assertNull(UnixStyleFileNameUtil.normalize("/parent/../../tmpl.wtl"));
+    }
+}
