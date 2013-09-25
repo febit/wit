0c224619698bdc525d01dc0acb9cd036497389f0
Zqq <zqq_90@163.com>
2013-09-25 08:04:19 +0800

    Added test for VisualVM

---
+++ webit-script/src/test/java/webit/script/TestForVisualVM.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2013, Webit Team. All Rights Reserved.
+package webit.script;
+
+import org.junit.Test;
+import webit.script.test.util.DiscardOutputStream;
+
+/**
+ *
+ * @author Zqq
+ */
+public class TestForVisualVM {
+
+    //@Test
+    public void test() {
+        int times = 10000;
+        Engine engine = EngineManager.getEngine();
+
+        try {
+            Template template = engine.getTemplate("/firstTmpl.wtl");
+            template.prepareTemplate();
+            DiscardOutputStream out = new DiscardOutputStream();
+            //TemplateAST result = template.prepareTemplate();
+            for (int i = 0; i < times; i++) {
+                template.merge(null, out);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
