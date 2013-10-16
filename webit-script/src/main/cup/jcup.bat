278db90b23ae2c6a0cd40832cce825d7938d47fc
Zqq <Zqq@Zqq-PC>
2013-10-16 15:03:32 +0800

    update java_cup tools

--- webit-script/src/main/cup/jcup.bat
+++ webit-script/src/main/cup/jcup.bat
@@ -7,7 +7,7 @@ rem set CLPATH=%JAVA_HOME%\lib\classes.zip;%JCUP_HOME%\java-cup-11a.jar
 
 rem %JAVA_HOME%\bin\java -classpath %CLPATH% java_cup.Main -parser Parser -symbols Tokens -interface -noscanner .\Parser.cup
 
-%JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -jar java_cup-0.12zqq.jar -parser Parser -symbols Tokens -runtimepackage webit.script.core.java_cup.runtime -interface -noscanner -nopositions -destdir ../java/webit/script/core Parser.cup
+%JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -jar java_cup_for_webit_script-20131016.jar -parser Parser -symbols Tokens -runtimepackage webit.script.core.java_cup.runtime -interface -noscanner -nopositions -destdir ../java/webit/script/core Parser.cup
 
 pause
 
---
+++ webit-script/src/main/cup/readme.txt
@@ -0,0 +1,24 @@
+1. debug RESULT
+[replace]
+//RESULT_DEBUG:
+=>
+
+
+2. Optimize 1:
+[replace reg]
+RESULT = ([^;]*?);[^;]*return RESULT;
+=>
+return $1;
+
+
+3. Optimize 2:
+
+like:
+webit.script.core.java_cup.runtime.Symbol stat$Symbol = CUP$Parser$stack.peek(0);
+Statment stat = (Statment) stat$Symbol.value;
+return stat;
+[replace reg]
+webit\.script\.core\.java_cup\.runtime\.Symbol ([a-zA-Z_$]*?)\$Symbol = CUP\$Parser\$stack\.peek\(([0-9]+)\);[\r\n\t\t ]*([a-zA-Z_$]*?) \1 = \(\3\) \1\$Symbol\.value;[\r\n\t\t ]*return \1;
+=>
+return CUP\$Parser\$stack.peek($2).value;
+
