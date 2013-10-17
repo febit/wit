ea156d48d68d68825877ad00122fd03a45d3d0d7
Zqq <Zqq@Zqq-PC>
2013-10-17 09:50:44 +0800

    copyright

--- webit-script/src/main/java/webit/script/util/BufferPeers.java
+++ webit-script/src/main/java/webit/script/util/BufferPeers.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Copyright (c) 2013, Webit Team. All Rights Reserved.
 package webit.script.util;
 
 import java.lang.ref.SoftReference;
