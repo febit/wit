06825c9cc29944c0040a95ba0b466bfeb5bb43b6
zqq90 <zqq_90@163.com>
2014-01-04 20:58:16 +0800

    Added LazyLoader

---
+++ webit-script/src/main/java/webit/script/loaders/impl/LazyLoader.java
@@ -0,0 +1,57 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.loaders.impl;
+
+import webit.script.Engine;
+import webit.script.Initable;
+import webit.script.exceptions.ResourceNotFoundException;
+import webit.script.loaders.Loader;
+import webit.script.loaders.Resource;
+import webit.script.loaders.impl.resources.LazyResource;
+import webit.script.util.ClassEntry;
+
+/**
+ *
+ * @since 1.4.0
+ * @author zqq90
+ */
+public class LazyLoader implements Loader, Initable {
+
+    protected int timeout;
+    protected ClassEntry _loader;
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public void setLoader(ClassEntry _loader) {
+        this._loader = _loader;
+    }
+    //
+    protected Loader loader;
+
+    public void init(Engine engine) {
+        try {
+            this.loader = (Loader) engine.getBean(_loader);
+        } catch (InstantiationException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public Resource get(String name) throws ResourceNotFoundException {
+        if (this.timeout > 0) {
+            return new LazyResource(this.loader.get(name), this.timeout);
+        } else {
+            return this.loader.get(name);
+        }
+    }
+
+    public String concat(String parent, String name) {
+        return this.loader.concat(parent, name);
+    }
+
+    public String normalize(String name) {
+        return this.loader.normalize(name);
+    }
+}
---
+++ webit-script/src/main/java/webit/script/loaders/impl/resources/LazyResource.java
@@ -0,0 +1,42 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.loaders.impl.resources;
+
+import java.io.IOException;
+import java.io.Reader;
+import webit.script.loaders.Resource;
+
+/**
+ *
+ * @since 1.4.0
+ * @author zqq90
+ */
+public class LazyResource implements Resource {
+
+    protected final int timeout;
+    protected final Resource resource;
+    private long expire;
+
+    public LazyResource(Resource resource, int timeout) {
+        this.timeout = timeout;
+        this.resource = resource;
+        this.expire = 0;
+    }
+
+    public boolean isModified() {
+        if (this.expire >= System.currentTimeMillis()) {
+            return false;
+        } else {
+            recalculateExpire();
+            return resource.isModified();
+        }
+    }
+
+    public Reader openReader() throws IOException {
+        recalculateExpire();
+        return this.resource.openReader();
+    }
+
+    private void recalculateExpire() {
+        this.expire = System.currentTimeMillis() + this.timeout;
+    }
+}
