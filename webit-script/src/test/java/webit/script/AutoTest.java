917cb9bc82d8c58fcb4d407ff5d3320e181a5177
zqq90 <zqq_90@163.com>
2014-01-05 10:05:00 +0800

    `AutoTest` support match out now.

--- webit-script/src/test/java/webit/script/AutoTest.java
+++ webit-script/src/test/java/webit/script/AutoTest.java
@@ -2,10 +2,13 @@
 package webit.script;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 import webit.script.exceptions.ParseException;
 import webit.script.exceptions.ResourceNotFoundException;
@@ -13,6 +16,8 @@ import webit.script.exceptions.ScriptRuntimeException;
 import webit.script.global.AssertGlobalRegister;
 import webit.script.test.util.DiscardOutputStream;
 import webit.script.util.ClassLoaderUtil;
+import webit.script.util.FastByteArrayOutputStream;
+import webit.script.util.FastByteBuffer;
 import webit.script.util.StringUtil;
 
 /**
@@ -21,23 +26,24 @@ import webit.script.util.StringUtil;
  */
 public class AutoTest {
 
+    private final static int BUFFER_SIZE = 1024;
     private final static String AUTO_TEST_PATH = "webit/script/test/tmpls/auto/";
 
-    private List<String> collectAutoTestTemplates() {
-        final List<String> templates = new LinkedList<String>();
+    private Map<String, String> collectAutoTestTemplates() {
+        final Map<String, String> templates = new TreeMap<String, String>();
 
         ClassLoader classLoader = ClassLoaderUtil.getDefaultClassLoader();
         try {
             URL url = classLoader.getResource(AUTO_TEST_PATH);
             File file = new File(url.getFile());
             String[] files = file.list();
-            for (int i = 0; i < files.length; i++) {
+            for (int i = 0, len = files.length; i < len; i++) {
                 String path = files[i];
                 if (StringUtil.endsWithIgnoreCase(path, ".wit")) {
-                    templates.add("/auto/" + path);
+                    String outPath = path.concat(".out");
+                    templates.put("/auto/".concat(path), new File(file, outPath).exists() ? outPath : null);
                 }
             }
-
         } catch (Exception ex) {
             //ignore
         }
@@ -45,24 +51,41 @@ public class AutoTest {
     }
 
     @Test
-    public void test() throws ResourceNotFoundException {
+    public void test() throws ResourceNotFoundException, IOException {
 
-        Engine engine = EngineManager.getEngine();
-        List<String> templates = collectAutoTestTemplates();
+        Map<String, String> templates = collectAutoTestTemplates();
+        ClassLoader classLoader = ClassLoaderUtil.getDefaultClassLoader();
 
-        AssertGlobalRegister assertGlobalRegister = AssertGlobalRegister.instance;
+        final FastByteBuffer bytesBuffer = new FastByteBuffer();
+        final byte[] buffer = new byte[BUFFER_SIZE];
+
         try {
 
-            for (Iterator<String> it = templates.iterator(); it.hasNext();) {
-                String templatePath = it.next();
-                DiscardOutputStream out = new DiscardOutputStream();
+            FastByteArrayOutputStream out = new FastByteArrayOutputStream();
+            for (Map.Entry<String, String> entry : templates.entrySet()) {
+                String templatePath = entry.getKey();
+                String outPath = entry.getValue();
+                if (outPath != null) {
+                    out.reset();
+                    mergeTemplate(templatePath, out);
 
-                System.out.println("AUTO RUN: " + templatePath);
-                Template template = engine.getTemplate(templatePath);
-                assertGlobalRegister.resetAssertCount();
-                template.merge(out);
+                    //read out 
+                    InputStream in;
+                    if ((in = classLoader.getResourceAsStream(AUTO_TEST_PATH.concat(outPath))) != null) {
+                        int read;
+                        bytesBuffer.clear();
+                        while ((read = in.read(buffer, 0, BUFFER_SIZE)) >= 0) {
+                            bytesBuffer.append(buffer, 0, read);
+                        }
+                        assertArrayEquals(bytesBuffer.toArray(), out.toByteArray());
+                        System.out.println("\tresult match to: " + outPath);
 
-                System.out.println("\tassert count: " + assertGlobalRegister.getAssertCount());
+                        bytesBuffer.clear();
+                    }
+                    out.reset();
+                } else {
+                    mergeTemplate(templatePath, new DiscardOutputStream());
+                }
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -71,5 +94,15 @@ public class AutoTest {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    public void mergeTemplate(String templatePath, OutputStream out) throws ResourceNotFoundException {
+
+        System.out.println("AUTO RUN: " + templatePath);
+        Template template = EngineManager.getEngine().getTemplate(templatePath);
+        AssertGlobalRegister.instance.resetAssertCount();
+        template.merge(out);
+
+        System.out.println("\tassert count: " + AssertGlobalRegister.instance.getAssertCount());
     }
 }
