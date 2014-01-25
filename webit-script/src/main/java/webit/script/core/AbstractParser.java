81dde21d66abec261863bc81e17535b400728ce7
zqq90 <zqq_90@163.com>
2014-01-25 14:19:57 +0800

    Updated java_cup tools.

--- webit-script/src/main/java/webit/script/core/AbstractParser.java
+++ webit-script/src/main/java/webit/script/core/AbstractParser.java
@@ -19,14 +19,15 @@ import webit.script.util.collection.ArrayStack;
 import webit.script.util.collection.Stack;
 
 /**
- * This class implements a skeleton table driven LR parser.
+ *
  * @author Zqq
  */
-abstract class lr_parser {
+abstract class AbstractParser {
 
     private final static int stackInitialCapacity = 24;
+    private final static int START_STATE = 0;
 
-    lr_parser() {
+    AbstractParser() {
         this._stack = new ArrayStack<Symbol>(stackInitialCapacity);
     }
     /**
@@ -95,47 +96,33 @@ abstract class lr_parser {
     }
 
     /**
-     * Perform a bit of user supplied action code (supplied by generated
-     * subclass). Actions are indexed by an internal action number assigned at
-     * parser generation time.
+     * Perform a bit of user supplied action code. Actions are indexed by an
+     * internal action number assigned at parser generation time.
      *
      * @param act_num the internal index of the action to be performed.
      * @return Object
      * @throws java.lang.Exception
      */
-    abstract Object do_action(int act_num) throws ParseException;
+    abstract Object doAction(int act_num) throws ParseException;
 
-    /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
     /**
-     * Fetch an action from the action table. The table is broken up into rows,
-     * one per state (rows are indexed directly by state number). Within each
-     * row, a list of index, value pairs are given (as sequential entries in the
-     * table), and the list is terminated by a default entry (denoted with a
-     * Symbol index of -1). To find the proper entry in a row we do a linear or
-     * binary search (depending on the size of the row).
      *
      * @param row actionTable[state]
      * @param id the Symbol index of the action being accessed.
      */
-    private short getAction(final short[] row, int sym) {
-        short tag;
+    private static short getAction(final short[] row, final int sym) {
         int first, last, probe, row_len;
-        //final short[] row = actionTable[state];
 
-        /* linear search if we are < 10 entries */
+        /* linear search if we are < 10 entries, otherwise binary search */
         if ((row_len = row.length) < 20) {
             for (probe = 0; probe < row_len; probe++) {
-                /* is this entry labeled with our Symbol or the default? */
-                tag = row[probe++];
-                if (tag == sym || tag == -1) {
-                    /* return the next entry */
+                if (row[probe++] == sym) {
                     return row[probe];
                 }
             }
         } else {
-            /* otherwise binary search */
             first = 0;
-            last = ((row_len - 1) >> 1) - 1;  /* leave out trailing default entry */
+            last = ((row_len - 1) >> 1);
 
             int probe_2;
             while (first <= last) {
@@ -149,43 +136,29 @@ abstract class lr_parser {
                     last = probe - 1;
                 }
             }
-
-            /* not found, use the default at the end */
-            return row[row_len - 1];
         }
-
-        /* shouldn't happened, but if we run off the end we return the 
-         default (error == 0) */
+        /* default: error == 0 */
         return 0;
     }
 
-    /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
     /**
-     * Fetch a state from the reduce-goto table. The table is broken up into
-     * rows, one per state (rows are indexed directly by state number). Within
-     * each row, a list of index, value pairs are given (as sequential entries
-     * in the table), and the list is terminated by a default entry (denoted
-     * with a Symbol index of -1). To find the proper entry in a row we do a
-     * linear search.
      *
      * @param row reduceTable[state]
      * @param id the Symbol index of the entry being accessed.
      */
-    private short getReduce(final short[] row, int sym) {
+    private static short getReduce(final short[] row, int sym) {
         int probe, len;
-        short tag;
-        for (probe = 0, len = row.length; probe < len; probe++) {
-            /* is this entry labeled with our Symbol or the default? */
-            if ((tag = row[probe++]) == sym || tag == -1) {
-                /* return the next entry */
-                return row[probe];
+        if (row != null) {
+            for (probe = 0, len = row.length; probe < len; probe++) {
+                if (row[probe++] == sym) {
+                    return row[probe];
+                }
             }
         }
-        /* if we run off the end we return the default (error == -1) */
+        //TODO: Error */
         return -1;
     }
 
-    /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
     /**
      * This method provides the main parsing routine. It returns only when
      * finishParsing() has been called (typically because the parser has
@@ -194,47 +167,45 @@ abstract class lr_parser {
      * and how the various tables are used.
      */
     private Symbol parse(final Lexer myLexer) throws Exception {
-        /* the current action code */
+
         int act;
-        Symbol cur_token;
+        Symbol currentToken;
         Symbol currentSymbol;
         final Stack<Symbol> stack;
         (stack = this._stack).clear();
-        //stack.push(newSymbol("START", 0, start_state()));
         {
             Symbol START;
-            (START = new Symbol(0, null)).state = Parser.START_STATE;
+            (START = new Symbol(0, null)).state = START_STATE;
             stack.push(currentSymbol = START);
         }
 
         final short[][] actionTable = Parser.ACTION_TABLE;
         final short[][] reduceTable = Parser.REDUCE_TABLE;
         final short[][] productionTable = Parser.PRODUCTION_TABLE;
-        //final Lexer myLexer = lexer;
-        /* get the first token */
-        cur_token = myLexer.nextToken();
+        
+        currentToken = myLexer.nextToken();
 
         /* continue until we are told to stop */
         goonParse = true;
         do {
 
             /* look up action out of the current state with the current input */
-            act = getAction(actionTable[currentSymbol.state], cur_token.id);
+            act = getAction(actionTable[currentSymbol.state], currentToken.id);
 
             /* decode the action -- > 0 encodes shift */
             if (act > 0) {
                 /* shift to the encoded state by pushing it on the _stack */
-                cur_token.state = act - 1;
-                stack.push(currentSymbol = cur_token);
+                currentToken.state = act - 1;
+                stack.push(currentSymbol = currentToken);
 
                 /* advance to the next Symbol */
-                cur_token = myLexer.nextToken();
+                currentToken = myLexer.nextToken();
             } else if (act < 0) {
                 /* if its less than zero, then it encodes a reduce action */
                 //reduceAction()
                 act = (-act) - 1;
                 final int symId, handleSize;
-                final Object result = do_action(act);
+                final Object result = doAction(act);
                 final short[] row;
                 symId = (row = productionTable[act])[0];
                 handleSize = row[1];
@@ -242,7 +213,7 @@ abstract class lr_parser {
                     currentSymbol = new Symbol(symId, result);
                 } else {
                     currentSymbol = new Symbol(symId, result, stack.peek(handleSize - 1)); //position based on left
-                        /* pops the handle off the _stack */
+                    //pop the handle
                     stack.pops(handleSize);
                 }
 
