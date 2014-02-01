b415a910baab43f6d401353f04a3e243bd87c5ed
zqq90 <zqq_90@163.com>
2014-02-01 14:25:51 +0800

    init webit-script-tools

---
+++ webit-script-tools/src/main/java/webit/script/tools/package-info.java
@@ -0,0 +1,2 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.tools;
---
+++ webit-script-tools/src/test/java/webit/script/tools/EngineManager.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.tools;
+
+import webit.script.Engine;
+import webit.script.Template;
+import webit.script.exceptions.ResourceNotFoundException;
+
+/**
+ *
+ * @author Zqq
+ */
+public class EngineManager {
+
+    private static final Engine engine;
+
+    static {
+        try {
+            engine = Engine.createEngine("/webit-script-lib-test.props", null);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static Engine getEngine() {
+        return engine;
+    }
+
+    public static Template getTemplate(String name) throws ResourceNotFoundException {
+        return engine.getTemplate(name);
+    }
+}
