ca1714709aeb06de8910e908e56671c2e31f9dfe
zqq90 <zqq_90@163.com>
2014-02-04 11:22:55 +0800

    Added lib-type.

---
+++ webit-script-tools/src/main/java/webit/script/tools/type/TypeGlobalRegister.java
@@ -0,0 +1,24 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.tools.type;
+
+import webit.script.global.GlobalManager;
+import webit.script.global.GlobalRegister;
+import webit.script.util.SimpleBag;
+
+/**
+ *
+ * @author zqq90
+ */
+public class TypeGlobalRegister implements GlobalRegister {
+
+    public void regist(GlobalManager manager) {
+        final SimpleBag constBag = manager.getConstBag();
+
+        constBag.set("is_array", TypeMethods.is_array);
+        constBag.set("is_bool", TypeMethods.is_bool);
+        constBag.set("is_function", TypeMethods.is_function);
+        constBag.set("is_callable", TypeMethods.is_function);
+        constBag.set("is_null", TypeMethods.is_null);
+        constBag.set("is_number", TypeMethods.is_number);
+    }
+}
---
+++ webit-script-tools/src/main/java/webit/script/tools/type/TypeMethods.java
@@ -0,0 +1,52 @@
+// Copyright (c) 2013-2014, Webit Team. All Rights Reserved.
+package webit.script.tools.type;
+
+import webit.script.Context;
+import webit.script.method.MethodDeclare;
+import webit.script.util.ArrayUtil;
+
+/**
+ *
+ * @author zqq90
+ */
+public class TypeMethods {
+
+    public final static MethodDeclare is_function = new MethodDeclare() {
+
+        public Object invoke(Context context, Object[] args) {
+            return ArrayUtil.get(args, 0, null) instanceof MethodDeclare;
+        }
+    };
+
+    public final static MethodDeclare is_number = new MethodDeclare() {
+
+        public Object invoke(Context context, Object[] args) {
+            return ArrayUtil.get(args, 0, null) instanceof Number;
+        }
+    };
+
+    public final static MethodDeclare is_bool = new MethodDeclare() {
+
+        public Object invoke(Context context, Object[] args) {
+            return ArrayUtil.get(args, 0, null) instanceof Boolean;
+        }
+    };
+
+    public final static MethodDeclare is_null = new MethodDeclare() {
+
+        public Object invoke(Context context, Object[] args) {
+            return args == null
+                    || args.length == 0
+                    || args[0] == null;
+        }
+    };
+
+    public final static MethodDeclare is_array = new MethodDeclare() {
+
+        public Object invoke(Context context, Object[] args) {
+            final Object item;
+            return (item = ArrayUtil.get(args, 0, null)) != null
+                    && item.getClass().isArray();
+        }
+    };
+}
