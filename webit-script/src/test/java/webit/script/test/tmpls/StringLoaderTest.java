679976c490ddaee1dc682bc7d06496c2e70688c7
zqq90 <zqq_90@163.com>
2014-03-17 18:39:14 +0800

    Added test of StringLoader.

---
+++ webit-script/src/test/java/webit/script/test/tmpls/StringLoaderTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webit.script.test.tmpls;
+
+import java.io.StringWriter;
+import org.junit.Test;
+import webit.script.Engine;
+import webit.script.exceptions.ResourceNotFoundException;
+
+/**
+ *
+ * @author Zqq
+ */
+public class StringLoaderTest {
+
+    @Test
+    public void test() throws ResourceNotFoundException {
+
+        final StringWriter writer = new StringWriter();
+
+        Engine.createEngine("")  
+                .getTemplate("string:<% echo \"Hello Webit Script！\"; %>")  
+                .merge(writer); 
+        System.out.println(writer);
+    }
+}
