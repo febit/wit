f25a7fa3e08c46854f0023161669b3241373716e
zqq90 <zqq_90@163.com>
2014-08-22 14:03:29 +0800

    fixed Convert

--- webit-script/src/main/java/webit/script/util/bean/Convert.java
+++ webit-script/src/main/java/webit/script/util/bean/Convert.java
@@ -12,9 +12,21 @@ import webit.script.util.StringUtil;
 public class Convert {
 
     public static Object convert(String string, Class cls) {
+        if (cls == String.class) {
+            return string;
+        }
+        if (cls == int.class) {
+            if (string == null || string.length() == 0) {
+                return 0;
+            }
+            return toInt(string);
+        } else if (cls == boolean.class) {
+            return toBoolean(string);
+        }
         if (string == null) {
             return null;
-        } else if (cls.isArray()) {
+        }
+        if (cls.isArray()) {
             if (cls == Class[].class) {
                 return toClassArray(string);
             } else if (cls == ClassEntry[].class) {
@@ -27,25 +39,26 @@ public class Convert {
                 return toIntegerArray(string);
             } else if (cls == boolean[].class) {
                 return toBoolArray(string);
-            } else if (cls == Integer[].class) {
+            } else if (cls == Boolean[].class) {
                 return toBooleanArray(string);
             }
-        } else if (cls == String.class) {
-            return string;
-        } else if (cls == Class.class) {
+        } else if (cls == Boolean.class) {
+            return toBoolean(string);
+        }
+        if (string.length() == 0) {
+            return null;
+        }
+        if (cls == Class.class) {
             return toClass(string);
         } else if (cls == ClassEntry.class) {
             return toClassEntry(string);
-        } else if (cls == int.class || cls == Integer.class) {
+        } else if (cls == Integer.class) {
             return toInt(string);
-        } else if (cls == boolean.class || cls == Boolean.class) {
-            return toBoolean(string);
         }
-
         return string;
     }
 
-    public static Class toClass(String string) {
+    private static Class toClass(String string) {
         try {
             return ClassUtil.getClass(string);
         } catch (ClassNotFoundException ex) {
@@ -53,15 +66,15 @@ public class Convert {
         }
     }
 
-    public static boolean toBoolean(String string) {
+    private static boolean toBoolean(String string) {
         return string != null && string.equalsIgnoreCase("true");
     }
 
-    public static int toInt(String string) {
+    private static int toInt(String string) {
         return Integer.valueOf(string);
     }
 
-    public static ClassEntry toClassEntry(String string) {
+    private static ClassEntry toClassEntry(String string) {
         try {
             return ClassEntry.wrap(string);
         } catch (ClassNotFoundException ex) {
@@ -69,13 +82,13 @@ public class Convert {
         }
     }
 
-    public static String[] toStringArray(String string) {
+    private static String[] toStringArray(String string) {
         String[] strings = StringUtil.splitc(string, ',');
         StringUtil.trimAll(strings);
         return strings;
     }
 
-    public static Class[] toClassArray(String string) {
+    private static Class[] toClassArray(String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final Class[] entrys = new Class[len];
@@ -85,7 +98,7 @@ public class Convert {
         return entrys;
     }
 
-    public static boolean[] toBoolArray(String string) {
+    private static boolean[] toBoolArray(String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final boolean[] entrys = new boolean[len];
@@ -95,7 +108,7 @@ public class Convert {
         return entrys;
     }
 
-    public static Boolean[] toBooleanArray(String string) {
+    private static Boolean[] toBooleanArray(String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final Boolean[] entrys = new Boolean[len];
@@ -105,7 +118,7 @@ public class Convert {
         return entrys;
     }
 
-    public static int[] toIntArray(String string) {
+    private static int[] toIntArray(String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final int[] entrys = new int[len];
@@ -115,7 +128,7 @@ public class Convert {
         return entrys;
     }
 
-    public static Integer[] toIntegerArray(String string) {
+    private static Integer[] toIntegerArray(String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final Integer[] entrys = new Integer[len];
@@ -125,7 +138,7 @@ public class Convert {
         return entrys;
     }
 
-    public static ClassEntry[] toClassEntryArray(final String string) {
+    private static ClassEntry[] toClassEntryArray(final String string) {
         final String[] strings = StringUtil.splitAndRemoveBlank(string);
         final int len = strings.length;
         final ClassEntry[] entrys = new ClassEntry[len];
