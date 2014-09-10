41c240820fcff3b3d28b6f4ac00a4dfc4176182b
zqq90 <zqq_90@163.com>
2014-09-10 11:11:36 +0800

    fixed Engine.UTF_8, should intern.

--- webit-script/src/main/java/webit/script/Engine.java
+++ webit-script/src/main/java/webit/script/Engine.java
@@ -33,7 +33,7 @@ import webit.script.util.StringUtil;
  */
 public final class Engine {
 
-    public static final String UTF_8 = "UTF-8";
+    public static final String UTF_8 = internEncoding("UTF-8");
 
     //settings
     private ClassEntry resourceLoaderType;
@@ -48,7 +48,7 @@ public final class Engine {
     private boolean shareRootData = true;
     private boolean trimCodeBlockBlankLine = true;
     private boolean appendLostSuffix;
-    private String suffix =  ".wit";
+    private String suffix = ".wit";
     private String encoding = UTF_8;
     private String inits;
     private String[] vars;
