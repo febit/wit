11f84717166c904d1e1851dcdecfe2459b07ab88
zqq90 <zqq_90@163.com>
2016-06-16 16:25:31 +0800

    Format AbstractParser

--- webit-script/src/main/java/webit/script/core/AbstractParser.java
+++ webit-script/src/main/java/webit/script/core/AbstractParser.java
@@ -34,6 +34,8 @@ import webit.script.util.StringUtil;
  */
 abstract class AbstractParser {
 
+    private static final Statement[] EMPTY_STATEMENTS = new Statement[0];
+
     //Self Operators
     static final int OP_PLUSEQ = 0;
     static final int OP_MINUSEQ = 1;
@@ -46,11 +48,6 @@ abstract class AbstractParser {
     static final int OP_ANDEQ = 8;
     static final int OP_XOREQ = 9;
     static final int OP_OREQ = 10;
-    private static final Statement[] EMPTY_STATEMENTS = new Statement[0];
-
-    private static final short[][] PRODUCTION_TABLE = loadData("Production");
-    private static final short[][] ACTION_TABLE = loadData("Action");
-    private static final short[][] REDUCE_TABLE = loadData("Reduce");
 
     private final Map<String, String> importedClasses;
     private final Map<String, Integer> labelsIndexMap;
@@ -62,13 +59,10 @@ abstract class AbstractParser {
     private boolean locateVarForce;
     private int currentLabelIndex;
 
-    final Stack<Symbol> symbolStack;
-    boolean goonParse;
     Template template;
     VariantManager varmgr;
 
     AbstractParser() {
-        this.symbolStack = new Stack<>(24);
         this.importedClasses = new HashMap<>();
         this.labelsIndexMap = new HashMap<>();
     }
@@ -122,8 +116,6 @@ abstract class AbstractParser {
             }
         }
     }
-
-    abstract Object doAction(int actionId) throws ParseException;
 
     boolean registClass(ClassNameBand classNameBand, int line, int column) throws ParseException {
         final String className = classNameBand.getClassSimpleName();
@@ -345,7 +337,7 @@ abstract class AbstractParser {
         return new TemplateAST(varmgr.getIndexers(), statements, varmgr.getVarCount());
     }
 
-    public static IBlock createIBlock(List<Statement> list, int varIndexer, int line, int column) {
+    static IBlock createIBlock(List<Statement> list, int varIndexer, int line, int column) {
         Statement[] statements = toStatementInvertArray(list);
         List<LoopInfo> loopInfoList = StatementUtil.collectPossibleLoopsInfo(statements);
         return loopInfoList != null
@@ -353,7 +345,7 @@ abstract class AbstractParser {
                 : new BlockNoLoops(varIndexer, statements, line, column);
     }
 
-    public static TryPart createTryPart(List<Statement> list, int varIndexer, int line, int column) {
+    static TryPart createTryPart(List<Statement> list, int varIndexer, int line, int column) {
         return new TryPart(createIBlock(list, varIndexer, line, column), line, column);
     }
 
@@ -510,179 +502,10 @@ abstract class AbstractParser {
         return StatementUtil.optimize(oper);
     }
 
-    private static short getAction(final short[] row, final int sym) {
-        final int len;
-        int probe;
-        /* linear search if we are < 10 entries, otherwise binary search */
-        if ((len = row.length) < 20) {
-            for (probe = 0; probe < len; probe++) {
-                if (row[probe++] == sym) {
-                    return row[probe];
-                }
-            }
-        } else {
-            int first, last;
-            first = 0;
-            last = ((len - 1) >> 1);
-
-            int probe_2;
-            while (first <= last) {
-                probe = (first + last) >> 1;
-                probe_2 = probe << 1;
-                if (sym == row[probe_2]) {
-                    return row[probe_2 + 1];
-                } else if (sym > row[probe_2]) {
-                    first = probe + 1;
-                } else {
-                    last = probe - 1;
-                }
-            }
-        }
-        //error
-        return 0;
-    }
-
-    private static short getReduce(final short[] row, int sym) {
-        if (row != null) {
-            for (int probe = 0, len = row.length; probe < len; probe++) {
-                if (row[probe++] == sym) {
-                    return row[probe];
-                }
-            }
-        }
-        //error
-        return -1;
-    }
-
-    private Symbol parse(final Lexer lexer) throws Exception {
-
-        int act;
-        Symbol currentToken;
-        Symbol currentSymbol;
-        final Stack<Symbol> stack = this.symbolStack;
-        stack.clear();
-
-        //Start Symbol
-        currentSymbol = new Symbol(0, -1, -1, null);
-        currentSymbol.state = 0;
-        stack.push(currentSymbol);
-
-        final short[][] actionTable = ACTION_TABLE;
-        final short[][] reduceTable = REDUCE_TABLE;
-        final short[][] productionTable = PRODUCTION_TABLE;
-
-        currentToken = lexer.nextToken();
-
-        goonParse = true;
-        do {
-
-            /* look up action out of the current state with the current input */
-            act = getAction(actionTable[currentSymbol.state], currentToken.id);
-
-            /* decode the action -- > 0 encodes shift */
-            if (act > 0) {
-                /* shift to the encoded state by pushing it on the _stack */
-                currentToken.state = act - 1;
-                stack.push(currentSymbol = currentToken);
-
-                /* advance to the next Symbol */
-                currentToken = lexer.nextToken();
-            } else if (act < 0) {
-                /* if its less than zero, then it encodes a reduce action */
-                act = (-act) - 1;
-                final int symId, handleSize;
-                final Object result = doAction(act);
-                final short[] row;
-                symId = (row = productionTable[act])[0];
-                handleSize = row[1];
-                if (handleSize == 0) {
-                    currentSymbol = new Symbol(symId, -1, -1, result);
-                } else {
-                    //position based on left
-                    currentSymbol = new Symbol(symId, result, stack.peek(handleSize - 1));
-                    //pop the handle
-                    stack.pops(handleSize);
-                }
-
-                /* look up the state to go to from the one popped back to */
- /* shift to that state */
-                currentSymbol.state = getReduce(reduceTable[stack.peek().state], symId);
-                stack.push(currentSymbol);
-
-            } else {
-                //act == 0
-                throw new ParseException(StringUtil.format("Syntax error at line {} column {}, Hints: {}", lexer.getLine(), lexer.getColumn(), getSimpleHintMessage(currentSymbol)), lexer.getLine(), lexer.getColumn());
-            }
-        } while (goonParse);
-
-        return stack.peek();
-    }
-
-    private static short[][] loadData(String name) {
-        ObjectInputStream in = null;
-        try {
-            in = new ObjectInputStream(ClassUtil.getDefaultClassLoader()
-                    .getResourceAsStream(StringUtil.concat("webit/script/core/Parser$", name, ".data")));
-            return (short[][]) in.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new Error(e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ioex) {
-                    // ignore
-                }
-            }
-        }
-    }
-
-    private static String getSimpleHintMessage(Symbol symbol) {
-        final short[] row = ACTION_TABLE[symbol.state];
-        final int len = row.length;
-        if (len == 0) {
-            return "[no hints]";
-        }
-        final boolean highterLevel = len > 8;
-        if (highterLevel && getAction(row, Tokens.SEMICOLON) != 0) {
-            return "forget ';' ?";
-        }
-        final StringBuilder sb = new StringBuilder();
-        boolean notFirst = false;
-        short sym;
-        for (int i = 0; i < len; i += 2) {
-            sym = row[i];
-            if (highterLevel && !isHintLevelOne(sym)) {
-                continue;
-            }
-            if (notFirst) {
-                sb.append(", ");
-            } else {
-                notFirst = true;
-            }
-            sb.append('\'')
-                    .append(symbolToString(sym))
-                    .append('\'');
-        }
-        return sb.toString();
-    }
-
-    private static boolean isHintLevelOne(short sym) {
-        switch (sym) {
-            case Tokens.COLON: //":"
-            case Tokens.SEMICOLON: //";"
-            case Tokens.RBRACE: //"}"
-            case Tokens.INTERPOLATION_END: //"}"
-            case Tokens.RPAREN: //")"
-            case Tokens.RBRACK: //"]"
-            case Tokens.IDENTIFIER: //"IDENTIFIER"
-            case Tokens.DIRECT_VALUE: //"DIRECT_VALUE"
-                return true;
-            default:
-                return false;
-        }
-    }
-
+    /* Base Parser */
+    private static final short[][] PRODUCTION_TABLE = loadData("Production");
+    private static final short[][] ACTION_TABLE = loadData("Action");
+    private static final short[][] REDUCE_TABLE = loadData("Reduce");
     private static final String[] SYMBOL_STRS = new String[]{
         "EOF", //EOF = 0
         "ERROR", //ERROR = 1
@@ -766,10 +589,188 @@ abstract class AbstractParser {
         "UNKNOWN"
     };
 
+    private static short getAction(final short[] row, final int sym) {
+        final int len;
+        int probe;
+        /* linear search if we are < 10 entries, otherwise binary search */
+        if ((len = row.length) < 20) {
+            for (probe = 0; probe < len; probe++) {
+                if (row[probe++] == sym) {
+                    return row[probe];
+                }
+            }
+        } else {
+            int first, last;
+            first = 0;
+            last = ((len - 1) >> 1);
+
+            int probe_2;
+            while (first <= last) {
+                probe = (first + last) >> 1;
+                probe_2 = probe << 1;
+                if (sym == row[probe_2]) {
+                    return row[probe_2 + 1];
+                } else if (sym > row[probe_2]) {
+                    first = probe + 1;
+                } else {
+                    last = probe - 1;
+                }
+            }
+        }
+        //error
+        return 0;
+    }
+
+    private static short getReduce(final short[] row, int sym) {
+        if (row != null) {
+            for (int probe = 0, len = row.length; probe < len; probe++) {
+                if (row[probe++] == sym) {
+                    return row[probe];
+                }
+            }
+        }
+        //error
+        return -1;
+    }
+
+    private static short[][] loadData(String name) {
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(ClassUtil.getDefaultClassLoader()
+                    .getResourceAsStream(StringUtil.concat("webit/script/core/Parser$", name, ".data")));
+            return (short[][]) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new Error(e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ioex) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    private static String getSimpleHintMessage(Symbol symbol) {
+        final short[] row = ACTION_TABLE[symbol.state];
+        final int len = row.length;
+        if (len == 0) {
+            return "[no hints]";
+        }
+        final boolean highterLevel = len > 8;
+        if (highterLevel && getAction(row, Tokens.SEMICOLON) != 0) {
+            return "forget ';' ?";
+        }
+        final StringBuilder sb = new StringBuilder();
+        boolean notFirst = false;
+        short sym;
+        for (int i = 0; i < len; i += 2) {
+            sym = row[i];
+            if (highterLevel && !isHintLevelOne(sym)) {
+                continue;
+            }
+            if (notFirst) {
+                sb.append(", ");
+            } else {
+                notFirst = true;
+            }
+            sb.append('\'')
+                    .append(symbolToString(sym))
+                    .append('\'');
+        }
+        return sb.toString();
+    }
+
+    private static boolean isHintLevelOne(short sym) {
+        switch (sym) {
+            case Tokens.COLON: //":"
+            case Tokens.SEMICOLON: //";"
+            case Tokens.RBRACE: //"}"
+            case Tokens.INTERPOLATION_END: //"}"
+            case Tokens.RPAREN: //")"
+            case Tokens.RBRACK: //"]"
+            case Tokens.IDENTIFIER: //"IDENTIFIER"
+            case Tokens.DIRECT_VALUE: //"DIRECT_VALUE"
+                return true;
+            default:
+                return false;
+        }
+    }
+
     private static String symbolToString(final short sym) {
         if (sym >= 0 && sym < SYMBOL_STRS.length) {
             return SYMBOL_STRS[sym];
         }
         return "UNKNOWN";
+    }
+
+    final Stack<Symbol> symbolStack = new Stack<>(24);
+    boolean goonParse;
+
+    abstract Object doAction(int actionId) throws ParseException;
+
+    Symbol parse(final Lexer lexer) throws Exception {
+
+        int act;
+        Symbol currentToken;
+        Symbol currentSymbol;
+        final Stack<Symbol> stack = this.symbolStack;
+        stack.clear();
+
+        //Start Symbol
+        currentSymbol = new Symbol(0, -1, -1, null);
+        currentSymbol.state = 0;
+        stack.push(currentSymbol);
+
+        final short[][] actionTable = ACTION_TABLE;
+        final short[][] reduceTable = REDUCE_TABLE;
+        final short[][] productionTable = PRODUCTION_TABLE;
+
+        currentToken = lexer.nextToken();
+
+        goonParse = true;
+        do {
+
+            /* look up action out of the current state with the current input */
+            act = getAction(actionTable[currentSymbol.state], currentToken.id);
+
+            /* decode the action -- > 0 encodes shift */
+            if (act > 0) {
+                /* shift to the encoded state by pushing it on the _stack */
+                currentToken.state = act - 1;
+                stack.push(currentSymbol = currentToken);
+
+                /* advance to the next Symbol */
+                currentToken = lexer.nextToken();
+            } else if (act < 0) {
+                /* if its less than zero, then it encodes a reduce action */
+                act = (-act) - 1;
+                final int symId, handleSize;
+                final Object result = doAction(act);
+                final short[] row;
+                symId = (row = productionTable[act])[0];
+                handleSize = row[1];
+                if (handleSize == 0) {
+                    currentSymbol = new Symbol(symId, -1, -1, result);
+                } else {
+                    //position based on left
+                    currentSymbol = new Symbol(symId, result, stack.peek(handleSize - 1));
+                    //pop the handle
+                    stack.pops(handleSize);
+                }
+
+                /* look up the state to go to from the one popped back to */
+ /* shift to that state */
+                currentSymbol.state = getReduce(reduceTable[stack.peek().state], symId);
+                stack.push(currentSymbol);
+
+            } else {
+                //act == 0
+                throw new ParseException(StringUtil.format("Syntax error at line {} column {}, Hints: {}", lexer.getLine(), lexer.getColumn(), getSimpleHintMessage(currentSymbol)), lexer.getLine(), lexer.getColumn());
+            }
+        } while (goonParse);
+
+        return stack.peek();
     }
 }
