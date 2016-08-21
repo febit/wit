f5df02eb2f90f2da8c41adb2f5da3edc8a3e052c
zqq90 <zqq_90@163.com>
2016-08-21 17:54:29 +0800

    Added Petite.

--- webit-script/src/main/java/webit/script/Engine.java
+++ webit-script/src/main/java/webit/script/Engine.java
@@ -1,16 +1,8 @@
 // Copyright (c) 2013-2015, Webit Team. All Rights Reserved.
 package webit.script;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import webit.script.core.NativeFactory;
@@ -26,9 +18,8 @@ import webit.script.loaders.Loader;
 import webit.script.loggers.Logger;
 import webit.script.resolvers.ResolverManager;
 import webit.script.security.NativeSecurityManager;
-import webit.script.util.ArrayUtil;
-import webit.script.util.ClassUtil;
 import webit.script.util.KeyValuesUtil;
+import webit.script.util.Petite;
 import webit.script.util.Props;
 import webit.script.util.PropsUtil;
 import webit.script.util.StringUtil;
@@ -42,10 +33,6 @@ public final class Engine {
     public static final String UTF_8 = internEncoding("UTF-8");
 
     private final ConcurrentMap<String, Template> templateCache = new ConcurrentHashMap<>();
-    private final Map<Class, Object> components = new HashMap<>();
-    private final Map<String, Object> beans = new HashMap<>();
-    private final Map<String, Object> datas = new HashMap<>();
-    private final Map<String, Entry> configEntrys = new HashMap<>();
 
     private boolean looseVar;
     private boolean shareRootData = true;
@@ -54,6 +41,7 @@ public final class Engine {
     private String inits;
     private String[] vars;
 
+    private Petite petite;
     private Logger logger;
     private Loader loader;
     private GlobalManager globalManager;
@@ -168,315 +156,42 @@ public final class Engine {
     }
 
     /**
-     * Get component or bean by type
+     * Get component or bean by type.
      *
      * @param <T>
      * @param type
      * @return Component
      * @since 2.0
      */
-    @SuppressWarnings("unchecked")
     public <T> T get(final Class<T> type) {
-        T bean = (T) components.get(type);
-        if (bean != null) {
-            return bean;
-        }
-        return (T) get(type.getName());
+        return this.petite.get(type);
     }
 
     /**
-     * Get bean by name
+     * Get bean by name.
      *
      * @param name
      * @return bean
      * @since 2.0
      */
     public Object get(final String name) {
-        Object bean = this.beans.get(name);
-        if (bean != null) {
-            return bean;
-        }
-        return createBeanIfAbsent(name);
-    }
-
-    public void config(Props props, Map<String, Object> parameters) {
-        if (props == null) {
-            props = new Props();
-        }
-        final Map<String, Object> extras;
-        if (parameters != null) {
-            extras = new HashMap<>();
-            for (Map.Entry<String, Object> entry : parameters.entrySet()) {
-                String key = entry.getKey();
-                if (key == null) {
-                    continue;
-                }
-                key = key.trim();
-                Object value = entry.getValue();
-                if (value instanceof String) {
-                    int len = key.length();
-                    if (len > 0) {
-                        if (key.charAt(len - 1) == '+') {
-                            props.append(key.substring(0, len - 1).trim(), (String) value);
-                        } else {
-                            props.set(key, (String) value);
-                        }
-                    }
-                } else {
-                    extras.put(key, value);
-                }
-            }
-        } else {
-            extras = null;
-        }
-
-        for (String key : props.keySet()) {
-            putProp(key, props.get(key));
-        }
-
-        if (extras != null) {
-            for (Map.Entry<String, Object> entrySet : extras.entrySet()) {
-                putProp(entrySet.getKey(), entrySet.getValue());
-            }
-        }
+        return this.petite.get(name);
     }
 
     public Object getConfig(String key) {
-        return this.datas.get(key);
-    }
-
-    private void putProp(String key, Object value) {
-        this.datas.put(key, value);
-        int index = key.lastIndexOf('.');
-        int index2;
-        if (index > 0
-                && (index2 = index + 1) < key.length()
-                && key.charAt(index2) != '@') {
-            String beanName = key.substring(0, index);
-            this.configEntrys.put(beanName,
-                    new Entry(key.substring(index2), value, this.configEntrys.get(beanName)));
-        }
+        return this.petite.getConfig(key);
     }
 
     public void addComponent(Object bean) {
-        //regist all impls
-        for (Class cls : ClassUtil.impls(bean.getClass())) {
-            this.components.put(cls, bean);
-        }
-    }
-
-    public void initComponents() {
-        addComponent(this);
-        Object globalRaw = datas.get("@global");
-        if (globalRaw != null) {
-            final String[] beanNames = StringUtil.toArray(globalRaw.toString());
-            final int size = beanNames.length;
-            if (size != 0) {
-                final Object[] globalBeans = new Object[size];
-                for (int i = 0; i < size; i++) {
-                    String name = beanNames[i];
-                    Object bean = newInstance(name);
-                    globalBeans[i] = bean;
-                    this.beans.put(name, bean);
-                    addComponent(bean);
-                }
-                inject("engine", this);
-                for (int i = 0; i < size; i++) {
-                    inject(beanNames[i], globalBeans[i]);
-                }
-            }
-        }
-        this.logger = get(Logger.class);
-    }
-
-    private Object newInstance(String key) {
-        String type;
-        do {
-            type = key;
-            key = (String) this.datas.get(key + ".@class");
-        } while (key != null);
-        return ClassUtil.newInstance(type);
-    }
-
-    private synchronized Object createBeanIfAbsent(String key) {
-        Object bean = this.beans.get(key);
-        if (bean != null) {
-            return bean;
-        }
-        bean = newInstance(key);
-        inject(key, bean);
-        this.beans.put(key, bean);
-        return bean;
-    }
-
-    public void inject(String key, final Object bean) {
-        LinkedList<String> keyList = new LinkedList<>();
-        do {
-            keyList.addFirst(key);
-            key = (String) this.datas.get(key + ".@class");
-        } while (key != null);
-
-        String[] keys = keyList.toArray(new String[keyList.size()]);
-
-        Map<String, Field> fields = ClassUtil.getSetableMemberFields(bean.getClass());
-        //global
-        for (Field field : fields.values()) {
-            Object comp = this.components.get(field.getType());
-            if (comp != null) {
-                try {
-                    field.set(bean, comp);
-                } catch (IllegalArgumentException | IllegalAccessException ex) {
-                    //shouldn't be
-                    throw new RuntimeException(ex);
-                }
-            }
-        }
-
-        Set<String> injected = new HashSet<>();
-        for (String profile : keys) {
-            inject(profile, bean, injected, fields);
-        }
-
-        //Init
-        for (Method method : bean.getClass().getMethods()) {
-            if (method.getAnnotation(Init.class) != null) {
-                final Class[] argTypes = method.getParameterTypes();
-                final Object[] args;
-                if (argTypes.length == 0) {
-                    args = ArrayUtil.EMPTY_OBJECTS;
-                } else {
-                    args = new Object[argTypes.length];
-                    for (int i = 0; i < argTypes.length; i++) {
-                        args[i] = this.components.get(argTypes[i]);
-                    }
-                }
-                try {
-                    method.invoke(bean, args);
-                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-                    //shouldn't be
-                    throw new RuntimeException(ex);
-                }
-            }
-        }
-    }
-
-    private void inject(final String beanName, final Object bean, final Set<String> injected, final Map<String, Field> fields) {
-
-        if (injected.contains(beanName)) {
-            return;
-        }
-        injected.add(beanName);
-        //inject @extends first
-        Object extendProfiles = datas.get(beanName.concat(".@extends"));
-        if (extendProfiles != null) {
-            for (String profile : StringUtil.toArray(String.valueOf(extendProfiles))) {
-                inject(profile, bean, injected, fields);
-            }
-        }
-
-        Entry entry = configEntrys.get(beanName);
-        while (entry != null) {
-            try {
-                Field field = fields.get(entry.name);
-                if (field == null) {
-                    if (logger != null) {
-                        logger.warn("Not found field {}#{} ", bean.getClass(), entry.name);
-                    }
-                } else {
-                    Object value = entry.value;
-                    if (value instanceof String) {
-                        value = convert((String) value, field.getType());
-                    }
-                    field.set(bean, value);
-                }
-            } catch (IllegalArgumentException | IllegalAccessException ex) {
-                throw new RuntimeException(ex);
-            }
-            entry = entry.next;
-        }
-    }
-
-    private Object convert(String string, Class cls) {
-        if (cls == String.class) {
-            return string;
-        }
-        if (cls == int.class) {
-            if (string == null || string.length() == 0) {
-                return 0;
-            }
-            return Integer.valueOf(string);
-        }
-        if (cls == boolean.class) {
-            return StringUtil.toBoolean(string);
-        }
-        if (string == null) {
-            return null;
-        }
-        if (cls.isArray()) {
-            final String[] strings = StringUtil.toArray(string);
-            final int len = strings.length;
-            if (cls == String[].class) {
-                return strings;
-            }
-            if (cls == Class[].class) {
-                final Class[] entrys = new Class[len];
-                for (int i = 0; i < len; i++) {
-                    entrys[i] = ClassUtil.getClass(strings[i]);
-                }
-                return entrys;
-            }
-            if (cls == int[].class) {
-                final int[] entrys = new int[len];
-                for (int i = 0; i < len; i++) {
-                    entrys[i] = Integer.valueOf(strings[i]);
-                }
-                return entrys;
-            }
-            if (cls == Integer[].class) {
-                final Integer[] entrys = new Integer[len];
-                for (int i = 0; i < len; i++) {
-                    entrys[i] = Integer.valueOf(strings[i]);
-                }
-                return entrys;
-            }
-            if (cls == boolean[].class) {
-                final boolean[] entrys = new boolean[len];
-                for (int i = 0; i < len; i++) {
-                    entrys[i] = StringUtil.toBoolean(strings[i]);
-                }
-                return entrys;
-            }
-            if (cls == Boolean[].class) {
-                final Boolean[] entrys = new Boolean[len];
-                for (int i = 0; i < len; i++) {
-                    entrys[i] = StringUtil.toBoolean(strings[i]);
-                }
-                return entrys;
-            }
-            Object[] array = (Object[]) Array.newInstance(cls.getComponentType(), len);
-            for (int i = 0; i < len; i++) {
-                array[i] = get(strings[i]);
-            }
-            return array;
-        } else {
-            if (cls == Boolean.class) {
-                return StringUtil.toBoolean(string);
-            }
-            if (string.length() == 0) {
-                return null;
-            }
-            if (cls == Class.class) {
-                return ClassUtil.getClass(string);
-            }
-            if (cls == Integer.class) {
-                return Integer.valueOf(string);
-            }
-            return get(string);
-        }
+        this.petite.addComponent(bean);
     }
 
     public boolean checkNativeAccess(String path) {
         return this.nativeSecurityManager.access(path);
+    }
+
+    public void inject(String key, final Object bean) {
+        this.petite.inject(key, bean);
     }
 
     public CoderFactory getCoderFactory() {
@@ -570,10 +285,11 @@ public final class Engine {
      */
     public static Engine create(final Props props, final Map<String, Object> parameters) {
 
-        final Engine engine = new Engine();
-        engine.config(props, parameters);
-        engine.initComponents();
+        final Petite petite = new Petite();
+        petite.config(props, parameters);
+        petite.initComponents();
 
+        final Engine engine = petite.get(Engine.class);
         engine.getLogger().info("Loaded props: {}", props.getModulesString());
         engine.executeInits();
         return engine;
@@ -591,19 +307,6 @@ public final class Engine {
             return Charset.forName(encoding).name();
         } catch (Exception e) {
             return encoding.intern();
-        }
-    }
-
-    private static final class Entry {
-
-        final String name;
-        final Object value;
-        final Entry next;
-
-        Entry(String name, Object value, Entry next) {
-            this.name = name;
-            this.value = value;
-            this.next = next;
         }
     }
 }
---
+++ webit-script/src/main/java/webit/script/util/Petite.java
@@ -0,0 +1,354 @@
+// Copyright (c) 2013-2015, Webit Team. All Rights Reserved.
+package webit.script.util;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import webit.script.Engine;
+import webit.script.Init;
+import webit.script.loggers.Logger;
+
+/**
+ * A simple dependency injection.
+ *
+ * @author zqq90
+ * @since 2.0
+ */
+public class Petite {
+
+    private final Map<Class, Object> components = new HashMap<>();
+    private final Map<String, Object> beans = new HashMap<>();
+    private final Map<String, Object> datas = new HashMap<>();
+    private final Map<String, Entry> entrys = new HashMap<>();
+
+    protected Logger logger;
+
+    /**
+     * Get component or bean by type.
+     *
+     * @param <T>
+     * @param type
+     * @return Component
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T get(final Class<T> type) {
+        T bean = (T) components.get(type);
+        if (bean != null) {
+            return bean;
+        }
+        return (T) get(type.getName());
+    }
+
+    /**
+     * Get bean by name.
+     *
+     * @param name
+     * @return bean
+     */
+    public Object get(final String name) {
+        Object bean = this.beans.get(name);
+        if (bean != null) {
+            return bean;
+        }
+        return resolveBeanIfAbsent(name);
+    }
+
+    private synchronized Object resolveBeanIfAbsent(String key) {
+        Object bean = this.beans.get(key);
+        if (bean != null) {
+            return bean;
+        }
+        bean = newInstance(key);
+        inject(key, bean);
+        this.beans.put(key, bean);
+        return bean;
+    }
+
+    public void config(Props props, Map<String, Object> parameters) {
+        if (props == null) {
+            props = new Props();
+        }
+        final Map<String, Object> extras;
+        if (parameters != null) {
+            extras = new HashMap<>();
+            for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+                String key = entry.getKey();
+                if (key == null) {
+                    continue;
+                }
+                key = key.trim();
+                Object value = entry.getValue();
+                if (value instanceof String) {
+                    int len = key.length();
+                    if (len > 0) {
+                        if (key.charAt(len - 1) == '+') {
+                            props.append(key.substring(0, len - 1).trim(), (String) value);
+                        } else {
+                            props.set(key, (String) value);
+                        }
+                    }
+                } else {
+                    extras.put(key, value);
+                }
+            }
+        } else {
+            extras = null;
+        }
+
+        for (String key : props.keySet()) {
+            setProp(key, props.get(key));
+        }
+
+        if (extras != null) {
+            for (Map.Entry<String, Object> entrySet : extras.entrySet()) {
+                setProp(entrySet.getKey(), entrySet.getValue());
+            }
+        }
+    }
+
+    public Object getConfig(String key) {
+        return this.datas.get(key);
+    }
+
+    private void setProp(String key, Object value) {
+        this.datas.put(key, value);
+        int index = key.lastIndexOf('.');
+        int index2;
+        if (index > 0
+                && (index2 = index + 1) < key.length()
+                && key.charAt(index2) != '@') {
+            String beanName = key.substring(0, index);
+            this.entrys.put(beanName,
+                    new Entry(key.substring(index2), value, this.entrys.get(beanName)));
+        }
+    }
+
+    public void addComponent(Object bean) {
+        //regist all impls
+        for (Class cls : ClassUtil.impls(bean.getClass())) {
+            this.components.put(cls, bean);
+        }
+    }
+
+    public void initComponents() {
+        addComponent(this);
+
+        final String[] beanNames = StringUtil.toArray((String) datas.get("@global"));
+        final int size = beanNames.length;
+        final Object[] globalBeans = new Object[size];
+        for (int i = 0; i < size; i++) {
+            String name = beanNames[i];
+            Object bean = newInstance(name);
+            globalBeans[i] = bean;
+            this.beans.put(name, bean);
+            addComponent(bean);
+        }
+        //FIXME: hard code! init Engine first.
+        inject("engine", get(Engine.class));
+        for (int i = 0; i < size; i++) {
+            if ("engine".equals(beanNames[i])) {
+                continue;
+            }
+            inject(beanNames[i], globalBeans[i]);
+        }
+        this.logger = get(Logger.class);
+    }
+
+    private Object newInstance(String key) {
+        String type;
+        do {
+            type = key;
+            key = (String) this.datas.get(key + ".@class");
+        } while (key != null);
+        return ClassUtil.newInstance(type);
+    }
+
+    public void inject(String key, final Object bean) {
+        LinkedList<String> keyList = new LinkedList<>();
+        do {
+            keyList.addFirst(key);
+            key = (String) this.datas.get(key + ".@class");
+        } while (key != null);
+
+        String[] keys = keyList.toArray(new String[keyList.size()]);
+
+        Map<String, Field> fields = ClassUtil.getSetableMemberFields(bean.getClass());
+        //global
+        for (Field field : fields.values()) {
+            Object comp = this.components.get(field.getType());
+            if (comp != null) {
+                try {
+                    field.set(bean, comp);
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    //shouldn't be
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+
+        Set<String> injected = new HashSet<>();
+        for (String profile : keys) {
+            inject(profile, bean, injected, fields);
+        }
+
+        invokeInitMethods(bean);
+    }
+
+    private void invokeInitMethods(Object bean) {
+        for (Method method : bean.getClass().getMethods()) {
+            if (method.getAnnotation(Init.class) == null) {
+                continue;
+            }
+            final Class[] argTypes = method.getParameterTypes();
+            final Object[] args;
+            if (argTypes.length == 0) {
+                args = ArrayUtil.EMPTY_OBJECTS;
+            } else {
+                args = new Object[argTypes.length];
+                for (int i = 0; i < argTypes.length; i++) {
+                    args[i] = this.components.get(argTypes[i]);
+                }
+            }
+            try {
+                method.invoke(bean, args);
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                //shouldn't be
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    private void inject(final String beanName, final Object bean, final Set<String> injected, final Map<String, Field> fields) {
+
+        if (injected.contains(beanName)) {
+            return;
+        }
+        injected.add(beanName);
+        //inject @extends first
+        Object extendProfiles = datas.get(beanName.concat(".@extends"));
+        if (extendProfiles != null) {
+            for (String profile : StringUtil.toArray(String.valueOf(extendProfiles))) {
+                inject(profile, bean, injected, fields);
+            }
+        }
+
+        Entry entry = entrys.get(beanName);
+        while (entry != null) {
+            try {
+                Field field = fields.get(entry.name);
+                if (field == null) {
+                    if (logger != null) {
+                        logger.warn("Not found field {}#{} ", bean.getClass(), entry.name);
+                    }
+                } else {
+                    Object value = entry.value;
+                    if (value instanceof String) {
+                        value = convert((String) value, field.getType());
+                    }
+                    field.set(bean, value);
+                }
+            } catch (IllegalArgumentException | IllegalAccessException ex) {
+                throw new RuntimeException(ex);
+            }
+            entry = entry.next;
+        }
+    }
+
+    private Object convert(String string, Class cls) {
+        if (cls == String.class) {
+            return string;
+        }
+        if (cls == int.class) {
+            if (string == null || string.length() == 0) {
+                return 0;
+            }
+            return Integer.valueOf(string);
+        }
+        if (cls == boolean.class) {
+            return StringUtil.toBoolean(string);
+        }
+        if (string == null) {
+            return null;
+        }
+        if (cls.isArray()) {
+            final String[] strings = StringUtil.toArray(string);
+            final int len = strings.length;
+            if (cls == String[].class) {
+                return strings;
+            }
+            if (cls == Class[].class) {
+                final Class[] entrys = new Class[len];
+                for (int i = 0; i < len; i++) {
+                    entrys[i] = ClassUtil.getClass(strings[i]);
+                }
+                return entrys;
+            }
+            if (cls == int[].class) {
+                final int[] entrys = new int[len];
+                for (int i = 0; i < len; i++) {
+                    entrys[i] = Integer.valueOf(strings[i]);
+                }
+                return entrys;
+            }
+            if (cls == Integer[].class) {
+                final Integer[] entrys = new Integer[len];
+                for (int i = 0; i < len; i++) {
+                    entrys[i] = Integer.valueOf(strings[i]);
+                }
+                return entrys;
+            }
+            if (cls == boolean[].class) {
+                final boolean[] entrys = new boolean[len];
+                for (int i = 0; i < len; i++) {
+                    entrys[i] = StringUtil.toBoolean(strings[i]);
+                }
+                return entrys;
+            }
+            if (cls == Boolean[].class) {
+                final Boolean[] entrys = new Boolean[len];
+                for (int i = 0; i < len; i++) {
+                    entrys[i] = StringUtil.toBoolean(strings[i]);
+                }
+                return entrys;
+            }
+            Object[] array = (Object[]) Array.newInstance(cls.getComponentType(), len);
+            for (int i = 0; i < len; i++) {
+                array[i] = get(strings[i]);
+            }
+            return array;
+        } else {
+            if (cls == Boolean.class) {
+                return StringUtil.toBoolean(string);
+            }
+            if (string.length() == 0) {
+                return null;
+            }
+            if (cls == Class.class) {
+                return ClassUtil.getClass(string);
+            }
+            if (cls == Integer.class) {
+                return Integer.valueOf(string);
+            }
+            return get(string);
+        }
+    }
+
+    private static final class Entry {
+
+        final String name;
+        final Object value;
+        final Entry next;
+
+        Entry(String name, Object value, Entry next) {
+            this.name = name;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
